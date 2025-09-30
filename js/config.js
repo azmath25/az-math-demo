02eb134b0a823dbe72b7337743d1acc1f84efd37
azmath25 <mathfolks23@gmail.com>
2025-09-30 07:41:09 +0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,2 @@
+// Replace with your Apps Script deployment URL
+const APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzPlxabMl4Hn3PjNWBymCj5I_eBTw8uJ4_by8TAKFQHKm3jNjKKay4rxKYwNL3OOEXi/exec";
