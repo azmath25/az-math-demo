38ae1564f5405ac828c05411582d31e5019f7223
azmath25 <mathfolks23@gmail.com>
2025-09-30 09:35:30 +0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // js/config.js
 // Replace with your deployed Apps Script web app exec URL
 const CONFIG = {
-  SCRIPT_URL: "YOUR_WEBAPP_URL" // example: "https://script.google.com/macros/s/AKfycb.../exec"
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyEYVysJpDm9CiPUy1oargHSOqCE0ZSJVQO8w1mmaNUGPOJ1ShL9VbYW88KZak4jdXN3A/exec" // example: "https://script.google.com/macros/s/AKfycb.../exec"
 };
